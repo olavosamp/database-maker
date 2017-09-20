9f3ec674cf9cef404745f68bd520dfda0821af7f
Olavo Sampaio <olavosamp@poli.ufrj.br>
2017-09-20 19:36:13 -0300

    Working on python port

--- python/getFrames.py
+++ python/getFrames.py
@@ -1,22 +1,35 @@
-import cv2
-import numpy as np
+import os
 
-videoPath = "F:\\Program Files\\Arquivos Incomuns\\Relevante\\UFRJ\\Projeto Final\\DadosPetrobras\\20170724_FTP83G_Petrobras\\CIMRL10-676_OK\PIDF-1 PO MRL-021_parte2.mpg"
+import cv2
+import numpy 		as np
+import pandas 		as pd
+
+videoPath = "C:\\Program Files\\Arquivos Incomuns\\Relevante\\UFRJ\\Projeto Final\\Petrobras\\PIDF-1 PO MRL-021_parte2.mpg"
 csvPath = "..\\csv\\PIDF-1 PO MRL-021_parte2 TEST.csv"
+
+data = pd.read_csv(csvPath)
 
 video = cv2.VideoCapture(videoPath)
 
 frameRate = np.ceil(video.get(cv2.CAP_PROP_FPS))
 
-framePeriod = 2000
+print("Frame rate", frameRate)
+
+framePeriod = (20/frameRate)*1000		# Interval between captured frames, in ms
 
 eventStart = 600000
 eventEnd   = 720000
-
 videoName = "testVideo1"
+ID = 1
+frameClass = "teste"
+
+# dirPath = "..\\images\\{}".format(videoName)
 dirPath = "..\\images\\{}".format(videoName)
 
-ID = 1
+try:
+	os.makedirs(dirPath)
+except OSError:
+	print("Directory already exists, probably")
 
 frameTime = eventStart
 frameCount = 1
@@ -25,15 +38,19 @@ while(frameTime < eventEnd):
 
 	errRead, frame = video.read()
 
-	cv2.imshow('frame', frame)
+	# cv2.imshow('frame', frame)
 	# cv2.waitKey(framePeriod)
 
-	imgPath = "{}\\{} ID{:2d} FRAME{:3d} class".format( dirPath, videoName, ID, frameCount)
-	print("\n", imgPath)
+	imgPath = "{}\\{} ID{:2d} FRAME{:3d} {}.jpg".format( dirPath, videoName, ID, frameCount, frameClass)
+	# print("\n", imgPath)
+
+	print("ID{:2d} Frame {:3d}".format(ID, frameCount))
+	errWrite = cv2.imwrite(imgPath, frame)
+	# print("Error write?: ", errWrite)
 
 	frameTime = frameTime + framePeriod
 	frameCount = frameCount + 1
 
-print("\n", frameRate)
+print("\nFrame rate: ", frameRate)
 
 video.release()
