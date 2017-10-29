eaf821524d6f9b9f51abec42f09ec93f644712a9
olavosamp <olavosamp@poli.ufrj.br>
2017-10-29 13:26:30 -0200

    Added two more csv files

--- python/run.py
+++ python/run.py
@@ -1,6 +1,8 @@
 from getFrames import getFrames
 
-videoPath = "F:\\Program Files\\Arquivos Incomuns\\Relevante\\UFRJ\\Projeto Final\\DadosPetrobras\\20170724_FTP83G_Petrobras\\GHmls16-263_OK\\DVD-1\\20161101225102250@DVR-SPARE_Ch1.wmv"
-csvPath = "F:\\Program Files\\Arquivos Incomuns\\Relevante\\UFRJ\\Projeto Final\\DadosPetrobras\\database-maker\\csv\\GHmls16-263_OK\\DVD-1\\20161101225102250@DVR-SPARE_Ch1.csv"
+videoPath = "F:\\Program Files\\Arquivos Incomuns\\Relevante\\UFRJ\\Projeto Final\\DadosPetrobras\\20170724_FTP83G_Petrobras\\GHmls16-263_OK\\DVD-1\\20161101235105625@DVR-SPARE_Ch1.wmv"
+csvPath = "F:\\Program Files\\Arquivos Incomuns\\Relevante\\UFRJ\\Projeto Final\\DadosPetrobras\\database-maker\\csv\\GHmls16-263_OK\\DVD-1\\20161101235105625@DVR-SPARE_Ch1.csv"
+# videoPath = "F:\\Program Files\\Arquivos Incomuns\\Relevante\\UFRJ\\Projeto Final\\DadosPetrobras\\20170724_FTP83G_Petrobras\\CIMRL10-676_OK\\PIDF-1 PO MRL-021_parte2.mpg"
+# csvPath = "F:\\Program Files\\Arquivos Incomuns\\Relevante\\UFRJ\\Projeto Final\\DadosPetrobras\\database-maker\\csv\\CIMRL10-676_OK\\PIDF-1 PO MRL-021_parte2.csv"
 
 frameTotal = getFrames(videoPath, csvPath)
